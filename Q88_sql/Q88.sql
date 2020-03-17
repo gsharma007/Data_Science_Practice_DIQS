f0fe09a82714635dc39179aaa4fa3e654b004443
Gaurav Sharma <sharmagaurav459@gmail.com>
2020-03-17 01:32:39 -0700

    Create Q88.sql

---
+++ Q88_sql/Q88.sql
@@ -0,0 +1,8 @@
+"""
+Given the following dataset, can you write a SQL query that returns the top 3 performing ad groups each day?
+
+Here we'll define performance as the ratio between revenue and spend (e.g. revenue / spend). 
+In other words, the higher the ratio the better the peformance. 
+The output of the query will be the date and an array of the ad groups.
+"""
+
