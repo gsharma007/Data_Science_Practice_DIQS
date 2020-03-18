1b932099642599021ad6d7684086fa5ca3bebc90
Gaurav Sharma <sharmagaurav459@gmail.com>
2020-03-18 01:05:02 -0700

    Update Q5.sql

--- Q05_sql/Q5.sql
+++ Q05_sql/Q5.sql
@@ -4,7 +4,6 @@
 You're consulting for a company, and you've sent out a survey that asks successive qustions randomly. 
 The survey logs data into a table called survey_logging. The schema of the table is:
 
-
 Column Name	Data Type	Description
 employee_id	integer	employee id of the survey respondant
 action	string	Will be one of the following values, 'view', 'answer', 'skip'
